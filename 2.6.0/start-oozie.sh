a5c479e14d56d213b4283a6a5f555c4ef1c4dcc1
Chris Fournier <chris.fournier@shopify.com>
2017-02-23 15:45:27 -0500

    Good to go

--- 2.6.0/start-oozie.sh
+++ 2.6.0/start-oozie.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+set -ex
 /usr/local/oozie-4.1.0/bin/oozie-setup.sh sharelib create -fs hdfs://hdfs-namenode:9000
-/usr/local/oozie-4.1.0/bin/oozied.sh start
+/usr/local/oozie-4.1.0/bin/oozied.sh run
